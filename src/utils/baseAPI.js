3a68567fd21e70560398e593bfc680a207bb28ad
兰小兰 <479323417@qq.com>
2019-12-18 21:45:57 +0800

    封装-wepy请求

--- src/utils/baseAPI.js
+++ src/utils/baseAPI.js
@@ -7,3 +7,23 @@ wepy.myToast = function (title, icon = 'none') {
     icon
   })
 }
+
+// 封装wepy的get请求
+
+const baesURL = 'https://uinav.com/api/public/v1'
+
+wepy.get = function (url, data = {}) {
+  return wepy.request({
+    url: baesURL + url,
+    method: 'GET',
+    data
+  })
+}
+
+wepy.post = function (url, data = {}) {
+  return wepy.request({
+    url: baesURL + url,
+    method: 'POST',
+    data
+  })
+}
